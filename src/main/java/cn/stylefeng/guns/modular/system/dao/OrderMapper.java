fa11402734f87bea2f0d8e9b2e8d53541482262e
yanghao <1710121398@vip.henu.edu.cn>
2019-07-22 20:52:02 +0800

    添加订单管理

---
+++ src/main/java/cn/stylefeng/guns/modular/system/dao/OrderMapper.java
@@ -0,0 +1,16 @@
+package cn.stylefeng.guns.modular.system.dao;
+
+import cn.stylefeng.guns.modular.system.model.Order;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 订单表 Mapper 接口
+ * </p>
+ *
+ * @author yanghao
+ * @since 2019-07-22
+ */
+public interface OrderMapper extends BaseMapper<Order> {
+
+}
